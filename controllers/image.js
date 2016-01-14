17c8e5c53954ddb4d9def8e5b3e24e476dfb593e
Andrew <andy_htun@hotmail.com>
2016-01-14 16:00:40 +0000

    MongoDB function added

--- controllers/image.js
+++ controllers/image.js
@@ -1,69 +1,85 @@
 var fs = require('fs'),
-    path = require('path');
+    path = require('path'),
+    sidebar = require('../helpers/sidebar'),
+    Models = require('../models');
 
 module.exports = {
   index: function(req, res) {
     var viewModel = {
-    image: {
-        uniqueId:       1,
-        title:          'Sample Image 1',
-        description:    'This is a sample.',
-        filename:       'sample1.jpg',
-        views:          0,
-        likes:          0,
-        timestamp:      Date.now()
-    },
-    comments: [
-        {
-            image_id:   1,
-            email:      'test@testing.com',
-            name:       'Test Tester',
-            gravatar:   'http://lorempixel.com/75/75/animals/1',
-            comment:    'This is a test comment...',
-            timestamp:  Date.now()
-        },{
-            image_id:   1,
-            email:      'test@testing.com',
-            name:       'Test Tester',
-            gravatar:   'http://lorempixel.com/75/75/animals/2',
-            comment:    'Another followup comment!',
-            timestamp:  Date.now()
-        }
-    ]
+    image: {},
+    comments: []
 };
-    res.render('image', viewModel);
+    Models.Image.findOne({ filename: { $regex:
+    req.params.image_id } },
+    function(err, image) {
+    if (err) { throw err; }
+    if (image) {
+      image.views = image.views + 1;
+      viewModel.image = image;
+      image.save();
+      Models.Comment.find({ image_id: image._id}, {}, { sort: { 'timestamp': 1 }},
+function(err, comments){
+if (err) { throw err; }
+
+viewModel.comments = comments;
+
+sidebar(viewModel, function(viewModel) {
+res.render('image', viewModel);
+        });
+    }
+);
+    } else {
+      res.redirect('/');
+    }
+    });
   },
+    // 
+    // sidebar(viewModel, function(viewModel){
+    //   res.render('image', viewModel);
+    // });
+
   create:function(req, res) {
     var saveImage = function() {
-      var possible = 'abcdefghijklmnopqrstuvwxyz0123456789',
-      imgUrl = '';
+    var possible = 'abcdefghijklmnopqrstuvwxyz0123456789',
+    imgUrl = '';
+    for(var i=0; i< 6; i+=1) {
+    imgUrl += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+    Models.Image.find({ filename: imgUrl }, function(err, images) {
+    if (images.length> 0) {
+    saveImage();
+            } else {
+    var tempPath = req.files.file.path,
+    ext = path.extname(req.files.file.name).toLowerCase(),
+    targetPath = path.resolve('./public/upload/temp/' + imgUrl + ext);
 
-  for(var i=0; i < 6; i+=1) {
-      imgUrl += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
+    if (ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif' || ext === '.img') {
+    fs.rename(tempPath, targetPath, function(err) {
+    if (err) { throw err; }
 
-  var tempPath = req.files.file.path,
-      ext = path.extname(req.files.file.name).toLowerCase(),
-      targetPath = path.resolve('./public/upload/temp/' + imgUrl + ext);
+    var newImg = new Models.Image({
+    title: req.body.title,
+    filename: imgUrl + ext,
+    description: req.body.description
+                       });
+    newImg.save(function(err, image) {
+    res.redirect('/images/' + image.uniqueId);
+                       });
+                    });
+                } else {
+    fs.unlink(tempPath, function () {
+    if (err) { throw err; }
 
-  if (ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif') {
-      fs.rename(tempPath, targetPath, function(err) {
-          if (err) throw err;
-
-          res.redirect('/images/' + imgUrl);
-      });
-  } else {
-      fs.unlink(tempPath, function () {
-          if (err) throw err;
-
-          res.json(500, {error: 'Only image files are allowed.'});
-      });
-  }
-};
-saveImage();
-},
+    res.json(500, {error: 'Only image files are allowed.'});
+                    });
+                }
+           }
+        });
+    };
+    saveImage();
+    },
   like: function(req, res) {
-    res.send('The image:like POST controller');
+    res.json({likes: 1});
   },
   comment: function(req, res) {
     res.send('The image:comment POST controller');
